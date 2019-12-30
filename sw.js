348cf9763d1ebc3a139a35f56692273dbd84c5e3
IvanLuLyf <835498692@qq.com>
2019-12-30 15:32:12 +0800

    Exam

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function (event) {
+    self.skipWaiting();
+});
+self.addEventListener('fetch', function (event) {
+    if (event.request.url.endsWith('sw.js')) {
+        return;
+    }
+    let headers = new Headers();
+    headers.set('content-type', 'text/html');
+    headers.set('power-by', 'BunnyFront');
+    let result = `
+<html>
+<head>
+<title>Bunny OneSite</title>
+</head>
+<body>
+    <h1>OneSite</h1>
+    <p>${event.request.url}</p>
+</body>
+</html>
+    `;
+    event.respondWith(new Response(result, {headers: headers}));
+});
